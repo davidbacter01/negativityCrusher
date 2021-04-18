18ac1733270cce33fc391c4b4639844d5271736d
davidbacter01 <davidbacter01@gmail.com>
2021-04-18 23:54:26 +0300

    spawn random enemies

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ const ctx = canvas.getContext("2d");
 const screenCenter = {x: canvas.width/2, y: canvas.height/2};
 const gameObjects = [];
 const projectiles = [];
+const enemies = [];
 
 class Player {
     constructor(x, y, color, radius, ctx){
@@ -30,13 +31,19 @@ class Player {
     shootTowards(x, y){
         // create a projectile
         const angle = Math.atan2(y-this.position.y, x-this.position.x);
-        console.log(angle);
+        let ySpeed = Math.sin(angle);
+        let xSpeed = Math.cos(angle);
+        // if (xSpeed > 0) {xSpeed +=1;}
+        // if (xSpeed < 0) {xSpeed -=1;}
+        // if (ySpeed > 0) {ySpeed +=1;}
+        // if (ySpeed < 0) {ySpeed -=1;}
+        
         projectiles.push(
             new Projectile(
                 this.position.x, 
                 this.position.y, 
-                Math.cos(angle), 
-                Math.sin(angle),
+                xSpeed, 
+                ySpeed,
                 this.ctx
                 )
              );
@@ -78,6 +85,30 @@ class Projectile{
     }
 }
 
+class Enemy{
+    constructor(spawnX, spawnY, xSpeed, ySpeed, ctx, radius){
+        this.radius = radius + 10;
+        this.ctx = ctx;
+        this.position = {x: spawnX, y: spawnY};
+        this.speed = {x: xSpeed, y: ySpeed};
+        this.color = '#2f477a';
+    }
+
+    draw(){
+        this.ctx.beginPath();
+        this.ctx.arc(this.position.x, this.position.y, this.radius, 0 ,2 * Math.PI);
+        this.ctx.fillStyle = this.color
+        this.ctx.fill()
+        this.ctx.closePath();
+    }
+
+    update(){
+        this.position.x += this.speed.x;
+        this.position.y += this.speed.y;
+        this.draw()
+    }
+}
+
 class InputHandler{
     constructor(player){
         this.player = player;
@@ -90,6 +121,26 @@ class InputHandler{
 const player = new Player(screenCenter.x, screenCenter.y, '#FF0000', 20, ctx);
 const inputHandler = new InputHandler(player);
 gameObjects.push(player);
+
+function spawnEnemies() {
+    setInterval(() =>{
+        let x;
+        let y;
+        const radius = Math.random() * 30;
+        if (Math.random() < 0.5) {
+            x=Math.random() < 0.5? Math.random() * canvas.width: 0;
+            y=Math.random() * canvas.height;
+        }else{
+            x=Math.random() * canvas.width;
+            y=Math.random() <0.5? Math.random() * canvas.height: 0;
+        }
+        const angle = Math.atan2(canvas.height/2 - y, canvas.width/2 - x);
+        let ySpeed = Math.sin(angle);
+        let xSpeed = Math.cos(angle);
+        enemies.push(new Enemy(x,y, xSpeed, ySpeed, ctx, radius));
+        console.log(enemies);
+    }, 1000);
+}
 
 function animate(){
     requestAnimationFrame(animate);
@@ -104,6 +155,9 @@ function animate(){
             console.log('is not on screen')
         }
     })
+
+    enemies.forEach(enemy => enemy.update());
 }
 
 animate();
+spawnEnemies();
